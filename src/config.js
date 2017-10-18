86a53df42e358b87a35887d5a8e475d5752717e6
Andrew Truex <andrewtruex@gmail.com>
2017-10-17 23:29:06 -0400

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyBdbuofObK8Njp6iqyAH4mEMtUdDbiix9k",
+    authDomain: "firechat-ajtruex.firebaseapp.com",
+    databaseURL: "https://firechat-ajtruex.firebaseio.com",
+    projectId: "firechat-ajtruex",
+    storageBucket: "firechat-ajtruex.appspot.com",
+    messagingSenderId: "999713767185"
+};
+
+export default config
+
